07724cae52294e6ad1281afbdbdd427f84e028a9
Lucía Costamagna <luci3220@hotmail.com>
2024-06-01 20:18:41 -0300

    diseño web

--- emploidApp/src/Components/CustomCard.js
+++ emploidApp/src/Components/CustomCard.js
@@ -1,10 +1,20 @@
-// Importar las librerías necesarias
+
 import React from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity,Platform,Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  Dimensions,
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
 import imageSource from "../../assets/for.jpg";
-const { width,height } = Dimensions.get("window");
+
+const { width, height } = Dimensions.get("window");
+
 const CustomCard = ({ title, text }) => {
   const navigation = useNavigation();
   const navigateToLogin = () => {
@@ -41,32 +51,59 @@ const styles = StyleSheet.create({
     elevation: 3,
     marginTop: "8%",
     ...Platform.select({
-     
       web: {
-        height: height > 1024 ? "80%" : width > 768 ? "60%" : "70%",
-     
+        height: height > 1024 ? "80%" : width > 768 ? "50%" : "70%",
+        width: width > 1024 ? "60%" : width > 768 ? "70%" : "90%",
+        marginHorizontal: "20%",
+       
       },
     }),
   },
   image: {
-    width: "40%",
-    height: 90,
+    width: "30%",
+    ...Platform.select({
+      web: {
+        width: "30%",
+        marginBottom:25,
+        marginTop: "1%",
+        height: "auto",
+      },
+    }),
+
+    height: "100%",
     marginBottom: "5%",
     marginTop: "5%",
+    resizeMode: "cover",
   },
   content: {
     padding: 8,
     marginTop: "5%",
     marginHorizontal: "2%",
     width: 220,
+    ...Platform.select({
+      web: {
+        width: "60%",
+        marginTop: "1%",
+      },
+    }),
   },
   title: {
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 5,
+    ...Platform.select({
+      web: {
+        fontSize: 21,
+      },
+    }),
   },
   text: {
     fontSize: 14,
+    ...Platform.select({
+      web: {
+        fontSize: 18,
+      },
+    }),
     color: "#444",
   },
 });
